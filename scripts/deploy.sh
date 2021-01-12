41ddcfdbe4d6c761f5e7d531b411ee45677144fd
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2021-01-12 10:45:17 +0000

    Added deploy/build and removed a duplicate setup scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+cd ~/QA-Final-Project/kubernetes
+kubectl apply -f database/database.yaml
+kubectl apply -f back-end/backend.yaml
+kubectl apply -f frontend/fontend.yaml
+kubectl apply -f nginx/nginx.yaml
--- scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd terraform 
-terraform init
-terraform apply -auto-approve
-az aks get-credentials --resource-group liveResources --name aks_cluster
-
