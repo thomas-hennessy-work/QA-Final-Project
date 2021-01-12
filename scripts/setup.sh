d231af6e76abfa04247418c788af8e4c643cbcb7
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2021-01-12 09:59:21 +0000

    adding setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,6 @@
-az aks get-credentials --resource-group liveResources --name aks_cluster
+#!/bin/bash
 cd terraform 
 terraform init
 terraform apply -auto-approve
+az aks get-credentials --resource-group liveResources --name aks_cluster
+
