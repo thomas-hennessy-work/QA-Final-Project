dca66b0ab29fbb6261fee4ad4c8010aecb76daa0
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2021-01-12 11:36:31 +0000

    added some install scripts testing if they work

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker-compose build
-docker-compose push
+sudo docker login
+sudo docker-compose down --rmi all
+sudo docker-compose build
+sudo docker-compose push
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+sudo docker run qateam1/springclinicresttest
+sudo docker run qateam1/springclinicangulartest
