cd070f25433c45987c3d2740380e401d051239a9
Iwan Moreton <me@iwanmoreton.com>
2021-01-12 15:32:33 +0000

    hotfix - change path in script

--- scripts/setup-live-environment.sh
+++ scripts/setup-live-environment.sh
@@ -4,4 +4,4 @@ terraform init
 terraform plan --var-file=live.tfvars -out=liveplan
 terraform apply "liveplan"
 az aks get-credentials --resource-group liveResources --name aks_cluster
-cd ~/QA-Final-Project
+cd ../../../
