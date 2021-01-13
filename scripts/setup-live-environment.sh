29741b2ad9b70c43d12e7adb013212c4220897d7
Iwan Moreton <me@iwanmoreton.com>
2021-01-13 18:17:21 +0000

    change test file

--- scripts/setup-live-environment.sh
+++ scripts/setup-live-environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd terraform/environments/live
-az group delete -n stagingResources -y --no-wait
+az group delete -n stagingResources -y 
 terraform init 
 terraform plan --var-file=live.tfvars -out=liveplan
 terraform apply "liveplan"
