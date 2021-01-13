f3ce525551cea50e8725743ad2db3691ee64369e
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2021-01-13 12:00:43 +0000

    modified karam conf and script and updated chromium browser cache

--- spring-petclinic-angular/installE2EPackages.sh
+++ spring-petclinic-angular/installE2EPackages.sh
@@ -6,7 +6,7 @@ sudo npm install -g @angular/cli
 
 npm install --save-dev @angular-devkit/build-angular
 
-sudo npm install -g typescript
+sudo npm install -g typescript@3.4
 npm link typescript
 
 ng e2e
