02dd7c5dc382366f24fc62e92667ff77bcb4862a
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2021-01-14 10:46:34 +0000

    modified e2e script to add file permisions

--- spring-petclinic-angular/installE2EPackages.sh
+++ spring-petclinic-angular/installE2EPackages.sh
@@ -1,24 +1,19 @@
-echo "npm install"
+sudo chown -R $USER /usr/lib/node_modules
+sudo chown -R $USER ../../QA-Final-Project
+
 npm install
 
-echo "sudo npm install -g protractor"
 sudo npm install -g protractor
 
 #prevent the google mesage from showing
-export NG_CLI_ANALYTICS=off off
-echo "sudo npm install -g @angular/cli"
+export NG_CLI_ANALYTICS=ci
 sudo npm install -g @angular/cli -n
 
-echo "npm install --save-dev @angular-devkit/build-angula"
 npm install --save-dev @angular-devkit/build-angular
 
-echo "sudo npm install -g typescript@3.4"
 sudo npm install -g typescript@3.4
-echo "npm link typescript"
 npm link typescript
 
-echo "ng config -g cli.warnings.versionMismatch false"
 ng config -g cli.warnings.versionMismatch false
 
-echo "ng e2e"
 ng e2e
