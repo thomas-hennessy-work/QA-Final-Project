df675f2a09ca6cb9969005295577ccf892f8447e
thomas-hennessy-work <thomas.louis.hennessy@gmail.com>
2021-01-14 10:15:20 +0000

    modified instalation script to prevent google mesage during instalation

--- spring-petclinic-angular/installE2EPackages.sh
+++ spring-petclinic-angular/installE2EPackages.sh
@@ -1,14 +1,24 @@
+echo "npm install"
 npm install
 
+echo "sudo npm install -g protractor"
 sudo npm install -g protractor
 
-sudo npm install -g @angular/cli
+#prevent the google mesage from showing
+export NG_CLI_ANALYTICS=off off
+echo "sudo npm install -g @angular/cli"
+sudo npm install -g @angular/cli -n
 
+echo "npm install --save-dev @angular-devkit/build-angula"
 npm install --save-dev @angular-devkit/build-angular
 
+echo "sudo npm install -g typescript@3.4"
 sudo npm install -g typescript@3.4
+echo "npm link typescript"
 npm link typescript
 
+echo "ng config -g cli.warnings.versionMismatch false"
 ng config -g cli.warnings.versionMismatch false
 
+echo "ng e2e"
 ng e2e
